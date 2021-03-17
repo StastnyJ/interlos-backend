25973eebf6703c6d9e1502104017ef7706d57399
StastnyJ <xstastnyj@gmail.com>
2021-03-17 15:52:01 +0100

    Initial commit

---
+++ src/main/java/com/stastnyjakub/interlos/InterlosApplication.java
@@ -0,0 +1,13 @@
+package com.stastnyjakub.interlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterlosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterlosApplication.class, args);
+	}
+
+}
